d1f069030ef1d32b6f47ef02ff4211200c0821bc
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-02-19 20:47:36 -0500

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { Route, Link, Switch } from 'react-router-dom';
+import Home from './Components/Home';
+import Form from './Components/Form';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <header>
+        <h1>Lambda Eats</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/pizza">Pizza</Link>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/pizza">
+          <Form />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
